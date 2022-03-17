35602af3ccf5e1de6513f67fe5f336c378293c1c
Kevin Chromik <kevin.chromik@exaring.de>
2022-03-17 10:53:55 +0100

    Initial Commit

---
+++ Password Generator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Password Generator
+//
+//  Created by Kevin Chromik on 17.03.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Password Generator/Password_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Password_GeneratorApp.swift
+//  Password Generator
+//
+//  Created by Kevin Chromik on 17.03.22.
+//
+
+import SwiftUI
+
+@main
+struct Password_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
